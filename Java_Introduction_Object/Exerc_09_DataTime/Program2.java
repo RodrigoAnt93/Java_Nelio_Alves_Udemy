9d9d7660572952eebc6d39967afd2453efd51069
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-06-15 22:51:12 -0300

    Create Program2.java

---
+++ Java_Introduction_Object/Exerc_09_DataTime/Program2.java
@@ -0,0 +1,35 @@
+package app;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		LocalDate d01 = LocalDate.parse("1993-08-06");
+		LocalDateTime d02 = LocalDateTime.parse("1993-08-06T01:30:26");
+		Instant d03 = Instant.parse("1993-08-06T01:30:26Z");	
+		
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+		DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
+		DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
+		
+		System.out.println(d01.format(fmt1));
+		System.out.println(fmt1.format(d01));
+		System.out.println(d01.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		System.out.println();
+		System.out.println(d02.format(fmt2));
+		System.out.println(d02.format(fmt1));
+		
+		System.out.println();
+		System.out.println(fmt3.format(d03));
+		System.out.println(d02.format(fmt4));
+		System.out.println(fmt5.format(d03));
+	}	
+}
