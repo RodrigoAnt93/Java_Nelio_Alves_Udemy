0b51bbaeb5f0422c3a37a7f85c5d2038276230a5
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-07-03 21:37:42 -0300

    Create BadSolution.java

---
+++ Java_Exception_Treatment/Exerc_02_Booking_Hotel/application/BadSolution.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+
+public class BadSolution {
+
+	public static void main(String[] args) throws ParseException {
+		//Programa feito mostrando uma solução ruim de como tratar exceções. É melhor que a versão anteriora, mas ainda tem defeitos.
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		System.out.print("Room number: ");
+		int number = sc.nextInt();
+		System.out.print("Check-in date (dd/mm/yyyy): ");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.print("Check-out date (dd/mm/yyyy): ");
+		Date checkOut = sdf.parse(sc.next());
+		
+		if(!checkOut.after(checkIn)) {
+			System.out.println("Error in reservation: Check-out date must be after check-in date");
+		}
+		else {
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+			System.out.println();
+			System.out.println("Enter date to update the reservation: ");
+			System.out.print("Check-in date (dd/mm/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/mm/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+			
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
+			}
+			else {
+				System.out.println("Reservation: " + reservation);
+			}
+			
+			}
+		
+		
+		sc.close();
+
+	}
+
+}
